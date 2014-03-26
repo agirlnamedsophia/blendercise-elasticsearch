9e968546b6b4dc78932d5e18af751fe5c3693d90
agirlnamedsophia <sophia.elizabeth.russell@gmail.com>
2014-03-26 09:19:41 -0400

    blendercising

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ElasticExample::Application.config.secret_key_base = '279de54617859bf5db37b2734dec74692c0005c512d2c92fdaee77b14f9bb60c45a1ac21f02e8295c9120679847cf9c6c2f9e070255c7b353215d58972cee08a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ElasticExample::Application.config.session_store :cookie_store, key: '_elastic_example_session'
